e887794e391ea5b8b6291b52b93c72a107ee1226
yemyinthtet <yemyinthtet99999@gmail.com>
2025-08-15 14:39:04 +0630

    reback commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -47,7 +47,7 @@ function Login() {
           className="border border-black px-6 py-2 rounded-md space-y-3 w-96"
         >
           <h1 className="text-2xl items-center text-blue-600 font-bold">
-            Messenger
+            Talkii
           </h1>
           <h2 className="text-2xl items-center">
             Login with your{" "}
--- url.js
+++ url.js
@@ -1,3 +1,3 @@
 // export const url = 'http://localhost:5002'
 
-export const url = 'http://192.168.1.13:5002'
+export const url = 'http://192.168.1.51:5002'
